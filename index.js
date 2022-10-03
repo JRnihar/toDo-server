2feff7101f671dde2116860965aec50301e46814
JRnihar <niharmonirulislam@gmail.com>
2022-10-03 13:36:04 +0600

    update 2

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const app = express();
+const express = require("express")
+const cors = require("cors")
+// const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT || 5000;
-const cors = require('cors');
-app.use(cors());
+require("dotenv").config()
+const app = express()
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
 app.use(express.json())
+
 
 
 //To-do-app
@@ -14,7 +19,7 @@ app.use(express.json())
 //appuser
 //WjhvR3jI7k5DXHY4
 
-const uri = "mongodb+srv://appuser:WjhvR3jI7k5DXHY4@cluster0.dctmt.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dctmt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
